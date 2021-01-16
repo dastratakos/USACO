6d1ad7921f6eb0603c23c52a6f45a0b13b117de8
Dean Stratakos <dastratakos@gmail.com>
2021-01-16 09:24:10 -0800

    Solve 2020 Feb Bronze SwapitySwap

--- 2020-Feb/Bronze/3-SwapitySwap/SwapitySwap.java
+++ 2020-Feb/Bronze/3-SwapitySwap/SwapitySwap.java
@@ -3,20 +3,20 @@ import java.util.*;
 
 public class SwapitySwap {
     public static void main(String[] args) throws IOException {
-        String test_dir = "./2020-Feb/Bronze/2-MadScientist/tests/";
+        String test_dir = "./2020-Feb/Bronze/3-SwapitySwap/tests/";
         List<File> files = listDir(new File(test_dir));
 
         int num_failed = 0;
         for (int i = 0; i < files.size(); i += 2) {
             Input in = readInput(files.get(i));
-            int expected_ans = readOutput(files.get(i + 1));
-            int num_swaps = doSwaps(in.n, in.a, in.b);
+            int[] expected_ans = readOutput(in.n, files.get(i + 1));
+            int[] final_order = swap(in.n, in.k, in.a_1, in.a_2, in.b_1, in.b_2);
 
-            if (expected_ans != num_swaps) {
+            if (!Arrays.equals(expected_ans, final_order)) {
                 num_failed++;
                 System.out.println("FAILED TEST CASE " + getTestCase(files.get(i)));
                 System.out.println("Expected: " + expected_ans);
-                System.out.println("Got:      " + num_swaps + "\n");
+                System.out.println("Got:      " + final_order + "\n");
             }
         }
 
@@ -55,16 +55,29 @@ public class SwapitySwap {
 
     public static Input readInput(File file) throws FileNotFoundException, IOException {
         BufferedReader br_in = new BufferedReader(new FileReader(file));
-        int n = Integer.parseInt(br_in.readLine());
-        char[] a = br_in.readLine().toCharArray();
-        char[] b = br_in.readLine().toCharArray();
+        
+        StringTokenizer st = new StringTokenizer(br_in.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br_in.readLine());
+        int a_1 = Integer.parseInt(st.nextToken());
+        int a_2 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br_in.readLine());
+        int b_1 = Integer.parseInt(st.nextToken());
+        int b_2 = Integer.parseInt(st.nextToken());
+       
         br_in.close();
-        return new Input(n, a, b);
+        return new Input(n, k, a_1, a_2, b_1, b_2);
     }
 
-    public static int readOutput(File file) throws FileNotFoundException, IOException {
+    public static int[] readOutput(int n, File file) throws FileNotFoundException, IOException {
         BufferedReader br_out = new BufferedReader(new FileReader(file));
-        int expected_ans = Integer.parseInt(br_out.readLine());
+        int[] expected_ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            expected_ans[i] = Integer.parseInt(br_out.readLine());
+        }
         br_out.close();
         return expected_ans;
     }
@@ -75,30 +88,51 @@ public class SwapitySwap {
         return Integer.parseInt(filename.split("\\.")[0]);
     }
 
-    public static int doSwaps(int n, char[] a, char[] b) {
-        int num_swaps = 0;
-        while(!new String(a).equals(new String(b))) {
-            num_swaps++;
-            int lhs = 0;
-            while(a[lhs] == b[lhs]) lhs++;
-            int rhs = n-1;
-            while(a[rhs] == b[rhs]) rhs--;
-            for(int i = lhs; i <= rhs; i++) {
-                if(a[i] == 'G') a[i] = 'H';
-                else a[i] = 'G';
-            }
+    public static int[] swap(int n, int k, int a_1, int a_2, int b_1, int b_2) {
+        int[] cows = new int[n];
+        for (int i = 0; i < n; i++) cows[i] = i + 1;
+        
+        int cycleSize = 0;
+        boolean sorted = true;
+        do {
+            cycleSize++;
+            reverse(cows, a_1, a_2);
+            reverse(cows, b_1, b_2);
+            sorted = true;
+            for (int i = 0; sorted && i < n; i++) sorted = cows[i] == i + 1;
+        } while(!sorted);
+
+        k %= cycleSize;
+
+        for (int i = 0; i < n; i++) cows[i] = i + 1;
+
+        for (int i = 0; i < k; i++) {
+            reverse(cows, a_1, a_2);
+            reverse(cows, b_1, b_2);
         }
-        return num_swaps;
+
+        return cows;
+    }
+
+    private static void reverse(int[] cows, int a, int b) {
+        while (a < b) {
+            int t = cows[a - 1];
+            cows[a - 1] = cows[b - 1];
+            cows[b - 1] = t;
+            a++;
+            b--;
+        }
     }
 }
 
 class Input {
-    int n;
-    char[] a;
-    char[] b;
-    Input(int n, char[] a, char[] b) {
+    int n, k, a_1, a_2, b_1, b_2;
+    Input(int n, int k, int a_1, int a_2, int b_1, int b_2) {
         this.n = n;
-        this.a = a;
-        this.b = b;
+        this.k = k;
+        this.a_1 = a_1;
+        this.a_2 = a_2;
+        this.b_1 = b_1;
+        this.b_2 = b_2;
     }
 }
