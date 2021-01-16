fcb5932d13e37a81aca4cdc493c06be8c4c33412
Dean Stratakos <dastratakos@gmail.com>
2021-01-16 10:48:37 -0800

    Solve 2020 Dec Silver RectangularPaster

--- 2020-Dec/Silver/2-RectangularPasture/RectangularPasture.java
+++ 2020-Dec/Silver/2-RectangularPasture/RectangularPasture.java
@@ -9,18 +9,18 @@ public class RectangularPasture {
         int num_failed = 0;
         for (int i = 0; i < files.size(); i += 2) {
             Input in = readInput(files.get(i));
-            String[] expected_ans = readOutput(in.n, files.get(i + 1));
-            String[] amounts = simulateAmounts(in.n, in.xs, in.ys, in.dirs);
+            long expected_ans = readOutput(files.get(i + 1));
+            long num_subsets = countSubsets(in.n, in.xs, in.ys);
             
-            if (expected_ans == amounts) {
+            if (expected_ans == num_subsets) {
                 System.out.println("PASSED TEST " + getTestCase(files.get(i)) + 
-                    "/" + files.size() / 2 + ": " + amounts + "\n");
+                    "/" + files.size() / 2 + ": " + num_subsets + "\n");
             } else {
                 num_failed++;
                 System.out.println("FAILED TEST " + getTestCase(files.get(i)) + 
                     "/" + files.size() / 2);
                 System.out.println("  Expected: " + expected_ans);
-                System.out.println("  Got:      " + amounts + "\n");
+                System.out.println("  Got:      " + num_subsets + "\n");
             }
         }
 
@@ -33,38 +33,34 @@ public class RectangularPasture {
         }
     }
 
-    public static String[] simulateAmounts(int n, int[] xs, int[] ys, char[] dirs) {
-        int[] answer = new int[n];
-        Arrays.fill(answer, Integer.MAX_VALUE);
-
-        List<Integer> differences = new ArrayList<>();
+    /*
+    Implemented using 2D prefix sums:
+    https://usaco.guide/silver/prefix-sums?lang=cpp#2d-prefix-sums.
+    */
+    public static long countSubsets(int n, int[] xs, int[] ys) {
+        int[][] sums = new int[n + 1][n + 1];
+        for (int j = 0; j < n; j++) {
+            sums[xs[j]][ys[j]]++;
+        }
+        for (int x = 0; x <= n; x++) {
+            for (int y = 0; y <= n; y++) {
+                if (x > 0) sums[x][y] += sums[x - 1][y];
+                if (y > 0) sums[x][y] += sums[x][y - 1];
+                if (x > 0 && y > 0) sums[x][y] -= sums[x - 1][y - 1];
+            }
+        }
+        long answer = n + 1;
         for (int j = 0; j < n; j++) {
             for (int k = j + 1; k < n; k++) {
-                differences.add(Math.abs(xs[k] - xs[j]));
-                differences.add(Math.abs(ys[k] - ys[j]));
+                answer += getSum(sums, Math.min(xs[j], xs[k]), Math.max(xs[j], xs[k]), 1, Math.min(ys[j], ys[k]))
+                        * getSum(sums, Math.min(xs[j], xs[k]), Math.max(xs[j], xs[k]), Math.max(ys[j], ys[k]), n);
             }
         }
+        return answer;
+    }
 
-        Collections.sort(differences);
-        for (int d : differences) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    if (dirs[j] == 'E' && dirs[k] == 'N' && xs[j] < xs[k] && ys[k] < ys[j]) {
-                        if (xs[j] + d == xs[k] && ys[k] + Math.min(answer[k], d) > ys[j]) {
-                            answer[j] = Math.min(answer[j], d);
-                        } else if (ys[k] + d == ys[j] && xs[j] + Math.min(answer[j], d) > xs[k]) {
-                            answer[k] = Math.min(answer[k], d);
-                        }
-                    }
-                }
-            }
-        }
-
-        String[] amounts = new String[n];
-        for (int i = 0; i < n; i++) {
-            amounts[i] = answer[i] == Integer.MAX_VALUE ? "Infinity" : Integer.toString(answer[i]);
-        }
-        return amounts;
+    public static int getSum(int[][] sums, int fromX, int toX, int fromY, int toY) {
+        return sums[toX][toY] - sums[fromX - 1][toY] - sums[toX][fromY - 1] + sums[fromX - 1][fromY - 1];
     }
 
     /* ********** UTILITIES ********** */
@@ -73,23 +69,31 @@ public class RectangularPasture {
         int n = Integer.parseInt(br_in.readLine());
         int[] xs = new int[n];
         int[] ys = new int[n];
-        char[] dirs = new char[n];
+        Integer[] cows = new Integer[n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br_in.readLine());
-            dirs[i] = st.nextToken().charAt(0);
             xs[i] = Integer.parseInt(st.nextToken());
             ys[i] = Integer.parseInt(st.nextToken());
+            cows[i] = i;
         }
+
+        // compress x and y coordinates
+        Arrays.sort(cows, Comparator.comparingInt(i -> xs[i]));
+        for (int x = 1; x <= n; x++) {
+            xs[cows[x - 1]] = x;
+        }
+        Arrays.sort(cows, Comparator.comparingInt(i -> ys[i]));
+        for (int y = 1; y <= n; y++) {
+            ys[cows[y - 1]] = y;
+        }
+
         br_in.close();
-        return new Input(n, xs, ys, dirs);
+        return new Input(n, xs, ys);
     }
 
-    public static String[] readOutput(int n, File file) throws FileNotFoundException, IOException {
+    public static long readOutput(File file) throws FileNotFoundException, IOException {
         BufferedReader br_out = new BufferedReader(new FileReader(file));
-        String[] expected_ans = new String[n];
-        for (int i = 0; i < n; i++) {
-            expected_ans[i] = br_out.readLine();
-        }
+        long expected_ans = Long.parseLong(br_out.readLine());
         br_out.close();
         return expected_ans;
     }
@@ -131,11 +135,9 @@ class Input {
     int n;
     int[] xs;
     int[] ys;
-    char[] dirs;
-    Input(int n, int[] xs, int[] ys, char[] dirs) {
+    Input(int n, int[] xs, int[] ys) {
         this.n = n;
         this.xs = xs;
         this.ys = ys;
-        this.dirs = dirs;
     }
 }
