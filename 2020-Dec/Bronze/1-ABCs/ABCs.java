b4ca45d95890fb112c77fa871f432f3e937141eb
Dean Stratakos <dastratakos@gmail.com>
2021-01-18 00:16:21 -0800

    Create sanity_check.py

--- 2020-Dec/Bronze/1-ABCs/ABCs.java
+++ 2020-Dec/Bronze/1-ABCs/ABCs.java
@@ -1,89 +1,15 @@
-import java.io.*;
 import java.util.*;
 
 public class ABCs {
-    public static void main(String[] args) throws IOException {
-        String test_dir = "./2020-Dec/Bronze/1-ABCs/tests/";
-        List<File> files = listDir(new File(test_dir));
-
-        int num_failed = 0;
-        for (int i = 0; i < files.size(); i += 2) {
-            int[] nums = readInput(files.get(i));
-            int[] expected_ans = readOutput(files.get(i + 1));
-            int[] abc = findABC(nums);
-
-            if (!Arrays.equals(expected_ans, abc)) {
-                num_failed++;
-                System.out.println("FAILED TEST " + getTestCase(files.get(i)));
-                System.out.println("Expected: " + expected_ans);
-                System.out.println("Got:      " + abc + "\n");
-            }
-        }
-
-        if (num_failed == 0) {
-            System.out.println("PASSED ALL TESTS (" + files.size() / 2 + "/" + files.size() / 2 + ")");
-        } else {
-            System.out.println("FAILED " + num_failed + "/" + files.size() / 2 + " TESTS");
-        }
-    }
-
-    public static int[] findABC(int[] nums) {
-        Arrays.sort(nums);
-        return new int[]{nums[0], nums[1], nums[6] - nums[0] - nums[1]};
-    }
-
-    /* ********** UTILITIES ********** */
-    public static int[] readInput(File file) throws FileNotFoundException, IOException {
-        BufferedReader br_in = new BufferedReader(new FileReader(file));
+    public static void main(String[] args) {
         int[] nums = new int[7];
-        StringTokenizer st = new StringTokenizer(br_in.readLine());
+        Scanner input = new Scanner(System.in);
         for (int i = 0; i < 7; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            nums[i] = input.nextInt();
         }
-        br_in.close();
-        return nums;
-    }
+        input.close();
 
-    public static int[] readOutput(File file) throws FileNotFoundException, IOException {
-        BufferedReader br_out = new BufferedReader(new FileReader(file));
-        int[] expected_ans = new int[3];
-        StringTokenizer st = new StringTokenizer(br_out.readLine());
-        for (int i = 0; i < 3; i++) {
-            expected_ans[i] = Integer.parseInt(st.nextToken());
-        }
-        br_out.close();
-        return expected_ans;
-    }
-
-    public static List<File> listDir(File dirName) {
-        List<File> files = new ArrayList<>();
-        File[] fileList = dirName.listFiles();
-        for (File file : fileList) {
-            if (file.isFile()) {
-                files.add(file);
-            } else if (file.isDirectory()) {
-                files.add(file);
-                listDir(file);
-            }
-        }
-        Collections.sort(files, new Comparator<File>() {
-            @Override
-            public int compare(File f1, File f2) {
-                int n1 = getTestCase(f1);
-                int n2 = getTestCase(f2);
-                if (n1 == n2) {
-                    return f1.getPath().endsWith(".in") ? -1 : 1;
-                } else {
-                    return n1 - n2;
-                }
-            }
-        });
-        return files;
-    }
-
-    public static int getTestCase(File file) {
-        String[] tokens = file.getPath().split("/");
-        String filename = tokens[tokens.length - 1];
-        return Integer.parseInt(filename.split("\\.")[0]);
+        Arrays.sort(nums);
+        System.out.println(nums[0] + " " + nums[1] + " " + (nums[6] - nums[0] - nums[1]));
     }
 }
---
+++ 2020-Dec/Bronze/1-ABCs/sanity_check.py
@@ -0,0 +1,77 @@
+import os
+import subprocess
+
+class styles:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def header(msg):
+    return f'+{"-" * (len(msg) + 2)}+\n| {msg} |\n+{"-" * (len(msg) + 2)}+'
+
+def success(msg):
+    return styles.OKGREEN + msg + styles.ENDC
+
+def failure(msg):
+    return styles.FAIL + msg + styles.ENDC
+
+def bold(msg):
+    return styles.BOLD + msg + styles.ENDC
+
+if __name__ == '__main__':
+    programs = [x for x in os.listdir(('/').join(__file__.split('/')[:-1])) if x.lower().endswith('.java')]
+    assert len(programs) == 1, 'Please make sure there is only one .java file in the directory. ' + str(programs)
+    program = programs[0]
+    dir = ('/').join(__file__.split('/')[:-1]) + '/'
+
+    num_tests = int(len(os.listdir(dir + 'tests')) / 2)
+
+    print('\n' + header(f'Running {num_tests} tests on {program}') + '\n')
+
+    num_successes = 0
+    for i in range(1, num_tests + 1):
+        f_in = open(dir + f'tests/{i}.in', 'r')
+        input = f_in.read().strip()
+        f_in.close()
+
+        f_in = open(dir + f'tests/{i}.out', 'r')
+        expected_output = f_in.read().strip()
+        f_in.close()
+
+        java_program = subprocess.run(
+            ['java', dir + program],
+            stdout=subprocess.PIPE,
+            text=True,
+            input=input
+            )
+        actual_output = java_program.stdout.strip()
+
+        ok = expected_output == actual_output
+        
+        if ok:
+            num_successes += 1
+            print(bold(success(f'PASSED test case {i}')))
+        else:
+            print(bold(failure(f'FAILED test case {i}')))
+        
+        print(bold('Input') + \
+            ('\n' if '\n' in input else '           ') + \
+            input)
+        print(bold('Expected answer') + \
+            ('\n' if '\n' in expected_output else ' ') + \
+            success(expected_output))
+        print(bold('Your answer') + \
+            ('\n' if '\n' in expected_output else '     ') + \
+            (success(actual_output) if ok else failure(actual_output)))
+        print()
+
+    if (num_tests - num_successes == 0):
+        print(bold(success(header(f'PASSED ALL TESTS ({num_tests}/{num_tests})'))))
+    else:
+        print(bold(failure(header(f'PASSED {num_successes}/{num_tests} TESTS'))))
