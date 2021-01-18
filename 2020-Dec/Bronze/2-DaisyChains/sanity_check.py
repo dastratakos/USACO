facb5d089266e9f8fd52829e603859db346d9734
Dean Stratakos <dastratakos@gmail.com>
2021-01-18 07:07:16 -0800

    Add error handling, update print formatting

--- 2020-Dec/Bronze/2-DaisyChains/sanity_check.py
+++ 2020-Dec/Bronze/2-DaisyChains/sanity_check.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 class styles:
     HEADER = '\033[95m'
@@ -24,6 +25,9 @@ def success(msg):
 def failure(msg):
     return styles.FAIL + msg + styles.ENDC
 
+def warning(msg):
+    return styles.WARNING + msg + styles.ENDC
+
 def bold(msg):
     return styles.BOLD + msg + styles.ENDC
 
@@ -33,13 +37,48 @@ def format(msg):
         return ('\n').join(lines[:3]) + '\n...\n' + ('\n').join(lines[-3:])
     return msg
 
-if __name__ == '__main__':
-    programs = [x for x in os.listdir(('/').join(__file__.split('/')[:-1])) if x.lower().endswith('.java')]
-    assert len(programs) == 1, 'Please make sure there is only one .java file in the directory. ' + str(programs)
-    program = programs[0]
+def getProgramAndDir():
     dir = ('/').join(__file__.split('/')[:-1]) + '/'
 
+    programs = [x for x in os.listdir(('/').join(__file__.split('/')[:-1])) if x.lower().endswith('.java')]
+    if len(programs) == 0:
+        print('Error: ' + warning('Please creata a .java file in the directory.'))
+        return None, dir
+    elif len(programs) > 1:
+        print('Error: ' + warning('Please make sure there is only one .java file in the directory. ' + str(programs)))
+        return None, dir
+    program = programs[0]
+
+    return program, dir
+
+def getNumTests():
+    if not os.path.exists(dir + 'tests') > 0:
+        print('Error: ' + warning('Please create a tests/ sub-directory.'))
+        return None
     num_tests = int(len(os.listdir(dir + 'tests')) / 2)
+    if num_tests == 0:
+        print('Error: ' + warning('Please include test files in the tests/ sub-directory.'))
+        return None
+    return num_tests
+
+def runSubprocess(args, input):
+    java_program = subprocess.run(
+        args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        input=input
+        )
+    out = java_program.stdout.strip()
+    err = java_program.stderr.strip()
+    return out, err
+
+if __name__ == '__main__':
+    program, dir = getProgramAndDir()
+    num_tests = getNumTests()
+
+    if not program or not num_tests:
+        sys.exit()
 
     print('\n' + header(f'Running {num_tests} tests on {program}') + '\n')
 
@@ -53,18 +92,10 @@ if __name__ == '__main__':
         expected_output = f_in.read().strip()
         f_in.close()
 
-        java_program = subprocess.run(
-            ['java', dir + program],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-            input=input
-            )
-        actual_output = java_program.stdout.strip()
-
-        ok = expected_output == actual_output
+        actual_output, err = runSubprocess(['java', dir + program], input)
+        passed = expected_output == actual_output
         
-        if ok:
+        if passed:
             num_successes += 1
             print(bold(success(f'PASSED test case {i}')) + '\n')
         else:
@@ -78,9 +109,9 @@ if __name__ == '__main__':
             format(success(expected_output)) + '\n')
         print(bold('Your answer') + \
             ('\n' if '\n' in expected_output else '     ') + \
-            format((success(actual_output) if ok else failure(actual_output))))
-        if java_program.stderr.strip():
-            print(failure(java_program.stderr.strip()))
+            format((success(actual_output) if passed else failure(actual_output))))
+        if err:
+            print(failure(err))
         print('\n' + cyan('=' * 60) + '\n')
 
     if (num_tests - num_successes == 0):
