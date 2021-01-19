db8b9c878c3dc6b878263515f42183d8a059d51b
Dean Stratakos <dastratakos@gmail.com>
2021-01-18 22:30:21 -0800

    Solve 2019 Dec Bronze WhereAmI

--- 2019-Dec/Bronze*/2-WhereAmI/WhereAmI.java
+++ 2019-Dec/Bronze*/2-WhereAmI/WhereAmI.java
@@ -4,28 +4,24 @@ public class WhereAmI {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        int k = input.nextInt();
-        String[] words = new String[n];
-        for (int i = 0; i < n; i++) {
-            words[i] = input.next();
-        }
+        String s = input.next();
         input.close();
 
-        String currLine = "";
-        int length = 0; // current length of line
-		for (String word : words) {
-            if ((length + word.length()) > k) {
-                System.out.println(currLine);
-                currLine = word;
-                length = word.length();
-            } else {
-                if (length > 0) {
-                    currLine = currLine + " ";
-                }
-                currLine = currLine + word;
-                length += word.length();
-            }
+        int k = 1;
+        while (dups(n, s, k)) k++;
+
+        System.out.println(k);
+    }
+
+    /*
+    Returns true if there is a duplicate using length k
+    */
+    public static boolean dups(int n, String s, int k) {
+        Set<String> X = new HashSet<>();
+        for (int i = 0; i <= n - k; i++) {
+            if (X.contains(s.substring(i, i + k))) return true;
+            X.add(s.substring(i, i + k));
         }
-        System.out.println(currLine);
+        return false;
     }
 }
